fba26fe24768863e049ab69614ecd72dd81b7d92
Christian Toledana <toledana.ian@gmail.com>
2024-05-09 18:29:46 +0800

    Refactor HomePage text formatting
    The formatting of the text in the HomePage component was simplified by condensing the previous multiline formatting into a single line for readability and maintainability.

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,9 +1,7 @@
 export const HomePage = () => {
   return (
     <>
-      <h1 className="text-3xl font-bold underline bg-red-500 text-yellow-500">
-        Hello world!
-      </h1>
+      <h1 className="text-3xl font-bold underline bg-red-500 text-yellow-500">Hello world!</h1>
     </>
   );
 };
