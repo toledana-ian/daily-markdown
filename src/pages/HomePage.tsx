7781c0f7ac591cded5524d59db947920dbf006fc
Christian Toledana <toledana.ian@gmail.com>
2024-05-09 18:28:39 +0800

    Update ESLint config and format HomePage component
    The ESLint configuration was updated to include the "prettier" plugin and made the associated adjustments. The "airbnb" plugin was removed and "plugin:prettier/recommended" was added to achieve better code readability. At the same time, the HomePage component was reformatted to match the new linting rules.

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,9 +1,9 @@
 export const HomePage = () => {
-    return (
-        <>
-            <h1 className="text-3xl font-bold underline bg-red-500 text-yellow-500">
-                Hello world!
-            </h1>
-        </>
-    );
-}
+  return (
+    <>
+      <h1 className="text-3xl font-bold underline bg-red-500 text-yellow-500">
+        Hello world!
+      </h1>
+    </>
+  );
+};
