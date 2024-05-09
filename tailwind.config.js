9bc9695070f7b27d44956d572170ca098a7344b7
Christian Toledana <toledana.ian@gmail.com>
2024-05-09 18:20:24 +0800

    Simplify content paths in Tailwind CSS config
    Simplified the paths specified in the 'content' property of the Tailwind CSS configuration file. It now uses a single directive "./src/**/*.tsx", which captures all .tsx files within the 'src' directory recursively.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 export default {
     content: [
-        "./src/App.tsx",
-        "./src/main.tsx",
-        "./src/**/*.{tsx}"
+        "./src/**/*.tsx"
     ],
     theme: {
         extend: {},
