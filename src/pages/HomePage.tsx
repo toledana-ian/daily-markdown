02cc2f1e7b7fd818e5f0168bef93e9774ed19a42
Christian Toledana <toledana.ian@gmail.com>
2024-05-09 18:11:20 +0800

    Replace App component with HomePage component
    In this commit, the existing App component and associated CSS files were removed from the project. The file structure was reorganised to introduce a new HomePage component under the pages directory, and the application's root is updated to render the new HomePage component. Changed CSS naming and structure.

---
+++ src/pages/HomePage.tsx
@@ -0,0 +1,9 @@
+export const HomePage = () => {
+    return (
+        <>
+            <h1 className="text-3xl font-bold underline bg-red-500 text-yellow-500">
+                Hello world!
+            </h1>
+        </>
+    );
+}
