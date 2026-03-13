ad2c64f94c05fb7aaef22c0e0edf2bb1c5570b7f
Christian Toledana <toledana.ian@gmail.com>
2026-03-13 16:41:14 +0800

    feat: add notFoundComponent to root route
    - Integrated `NotFoundPage` component as the `notFoundComponent` in `createRootRoute`.
    - Enhanced routing to handle undefined routes with a 404 page.

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,6 +1,8 @@
 import { createRootRoute } from '@tanstack/react-router'
 import { DefaultLayout } from '@/app/layouts/DefaultLayout'
+import { NotFoundPage } from '@/features/home/pages/404'
 
 export const Route = createRootRoute({
   component: DefaultLayout,
+  notFoundComponent: NotFoundPage,
 })
