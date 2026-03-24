d053caafb40aa4af96c7454020d7be8b48a94795
Christian Toledana <toledana.ian@gmail.com>
2026-03-24 19:38:11 +0800

    refactor(main): reorder CSS import for improved readability

--- apps/web/src/main.tsx
+++ apps/web/src/main.tsx
@@ -1,8 +1,9 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import './index.css';
 import { createRouter, RouterProvider } from '@tanstack/react-router';
 import { routeTree } from './routeTree.gen';
+
+import './index.css';
 
 const router = createRouter({ routeTree });
 
